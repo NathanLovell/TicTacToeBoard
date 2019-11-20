659894e4a9d7c8fcd66300e7b5eb8d936d5d285e
NathanLovell <nslovell@mail.csuchico.edu>
2019-11-20 11:34:18 -0800

    initial commit

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -4,19 +4,26 @@
 
 #include <gtest/gtest.h>
 #include "TicTacToeBoard.h"
- 
+
 class TicTacToeBoardTest : public ::testing::Test
 {
-	protected:
-		TicTacToeBoardTest(){} //constructor runs before each test
-		virtual ~TicTacToeBoardTest(){} //destructor cleans up after tests
-		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor) 
+protected:
+	TicTacToeBoardTest() {}			 //constructor runs before each test
+	virtual ~TicTacToeBoardTest() {} //destructor cleans up after tests
+	virtual void SetUp() {}			 //sets up before each test (after constructor)
+	virtual void TearDown() {}		 //clean up after each test, (before destructor)
 };
 
-/* EXAMPLE TEST FORMAT
-TEST(TicTacToeBoardTest, unitTestName)
+EXAMPLE TEST FORMAT
+	TEST(TicTacToeBoardTest, unitTestName)
 {
 	ASSERT_TRUE(true);
+}
+
+/*
+TEST(TicTacToeBoardTest, toggleTurn)
+{
+	TicTacToeBoard tictactoeboard;
+	ASSERT_eq
 }
 */
