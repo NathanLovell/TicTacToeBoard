f9384790d2afe58e362d618ab2f5f2c1819880c7
NathanLovell <nslovell@mail.csuchico.edu>
2019-12-04 18:21:19 -0800

    add more tests for get piece and place piece

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -45,3 +45,75 @@ TEST(TicTacToeBoardTest, placeXIn)
 	TicTacToeBoard board;
 	ASSERT_EQ(board.placePiece(1, 1), X);
 }
+
+TEST(TicTacToeBoardTest, placeXOutSmall)
+{
+	TicTacToeBoard board;
+	ASSERT_EQ(board.placePiece(-1, -1), Invalid);
+}
+
+TEST(TicTacToeBoardTest, placeXOutLargeColumn)
+{
+	TicTacToeBoard board;
+	ASSERT_EQ(board.placePiece(1, 5), Invalid);
+}
+
+TEST(TicTacToeBoardTest, placeXOutLargeRow)
+{
+	TicTacToeBoard board;
+	ASSERT_EQ(board.placePiece(5, 1), Invalid);
+}
+
+TEST(TicTacToeBoardTest, getPieceX)
+{
+	TicTacToeBoard board;
+	board.placePiece(1, 1);
+	ASSERT_EQ(board.getPiece(1, 1), X);
+}
+
+TEST(TicTacToeBoardTest, getPieceIn)
+{
+	TicTacToeBoard board;
+	ASSERT_EQ(board.placePiece(1, 1), Blank);
+}
+
+TEST(TicTacToeBoardTest, getPieceOut)
+{
+	TicTacToeBoard board;
+	ASSERT_EQ(board.placePiece(1, 3), Invalid);
+}
+
+TEST(TicTacToeBoardTest, placeOneExisting)
+{
+	TicTacToeBoard board;
+	board.placePiece(1, 1);
+	ASSERT_EQ(board.placePiece(1, 1), X);
+}
+
+TEST(TicTacToeBoardTest, placeAfterWin)
+{
+	TicTacToeBoard board;
+	board.placePiece(0, 0);
+	board.placePiece(1, 0);
+	board.placePiece(0, 1);
+	board.placePiece(1, 1);
+	board.placePiece(0, 2);
+	board.placePiece(1, 2);
+	ASSERT_EQ(board.placePiece(1, 1), O);
+}
+
+TEST(TicTacToeBoardTest, placeAfterDraw)
+{
+	TicTacToeBoard board;
+	board.placePiece(0, 0);
+	board.placePiece(1, 0);
+	board.placePiece(1, 1);
+	board.placePiece(2, 2);
+	board.placePiece(2, 0);
+	board.placePiece(0, 2);
+	board.placePiece(1, 2);
+	board.placePiece(0, 1);
+	board.placePiece(2, 1);
+	ASSERT_EQ(board.placePiece(0, 0), X);
+}
+
